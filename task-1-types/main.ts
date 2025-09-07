74dbea4e4ab3351ed8c0541256b24c16c0352c1f
Karlene Williams-Pichler <karlene.williams-pichler@dci-student.org>
2025-09-08 00:06:42 +0200

    Completed Task-1-types

---
+++ task-1-types/main.ts
@@ -0,0 +1,19 @@
+const album = {
+    albumName: "The Final Countdown",
+    artist: "Europe",
+    isEpic: true,
+    year: 1986,
+    tracks: ["The Final Countdown",
+                "Rock the Night", 
+                "Carrie",
+                "Danger on the Track",
+                "Ninja",
+                "Heart of Stone",
+                "On the Loose",
+                "Love Chaser",
+                "Cherokee",
+                "Time Has Come"
+    ]
+};
+
+console.log(album);
