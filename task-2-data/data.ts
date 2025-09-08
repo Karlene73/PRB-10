d8dd5fde24148fefeba688d727df62f0843ffdf0
Karlene Williams-Pichler <karlene.williams-pichler@dci-student.org>
2025-09-08 02:00:19 +0200

    Completed Task-2-data

--- task-2-data/data.ts
+++ task-2-data/data.ts
@@ -2,45 +2,45 @@ import prompt from 'picoprompt'
 
 console.log("\n💻 Number examiner 💻\n")
 
-function bin(n) {
+function bin(n:number): string {
     return n.toString(2)
 }
 
-function oct(n) {
+function oct(n:number): string {
     return n.toString(8)
 }
 
-function hex(n) {
+function hex(n:number): string {
     return n.toString(16)
 }
 
-function padRight(s, n) {
-    if (s.length > n) {
-        return s
+function padRight(str: string, length: number): string {
+    if (str.length >= length) {
+        return str;
     }
 
-    let out = s
-    for (let i = s.length; i <= n; i++) {
-        out += " "
+    let out = str
+    for (let i = str.length; i < length; i++) {
+        out += " ";
     }
 
     return out;
 }
 
 while (true) {
-    const input = prompt("Number to examine: ")
-    const base10 = parseInt(input, 10)
-    if (isNaN(base10)) {
+    const input: string | null = prompt("Number to examine: ");
+    if (input === null || isNaN(parseInt(input, 10))) {
         break
     }
 
-    const base2 = bin(base10)
-    const base8 = oct(base10)
-    const base16 = hex(base10)
-    const x = base2.length - 1
+    const base10: number = parseInt(input, 10);
+    const base2: string = bin(base10);
+    const base8: string = oct(base10);
+    const base16: string = hex(base10);
+    const x: number = base2.length - 1;
 
-    console.log(`${input} is ${padRight(base2, x)} in base 2`)
-    console.log(`${input} is ${padRight(base8, x)} in base 8`)
-    console.log(`${input} is ${padRight(base16, x)} in base 16`)
-    console.log("\n")
+    console.log(`${input} is ${padRight(base2, x)} in base 2`);
+    console.log(`${input} is ${padRight(base8, x)} in base 8`);
+    console.log(`${input} is ${padRight(base16, x)} in base 16`);
+    console.log("\n");
 }
